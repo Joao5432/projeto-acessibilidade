f3f88f738b751b49fdfeb5334fc5880e89ad5215
Maria Fernanda <fernandamariacaetanopereira@gmail.com>
2023-11-03 14:36:05 -0300

    Atualizações

--- js/main.js
+++ js/main.js
@@ -7,7 +7,25 @@ if (formulario){
 }
 
 function validar(formulario){
+    //não deixar a página recarregar quando der submit
     
-    formulario.preventDefault();
-    alert("oiii")
+    //pegando os campos para validação e colocando em um array
+    const Listacampos = document.querySelectorAll(".validar");
+
+    //iterando campos de validacao
+    for (let i = 0; i < Listacampos.length; i++){
+        
+        //receber os campos completos
+        const campoCompleto = Listacampos[i];
+
+        //pegar os valores dos campos
+        var campoValor = campoCompleto.value;
+        console.log(campoValor);
+
+        if (campoValor === "") {
+            formulario.preventDefault();
+            document.getElementById("aviso").innerText = "oiii"
+        }
+
+    }
 }
